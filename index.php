7a9bb25f7c6d71b47fe6b2e1461cce40d177ae3d
Sai Main <saimain.venuslab@gmail.com>
2022-04-29 22:32:10 +0630

    init commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+header('Content-type: application/json');
+
+$url = "https://www.twse.com.tw/rsrc/data/zh/home/marquee.json";
+
+$client = new Client([
+    'base_uri' => $url
+]);
+
+$response = $client->request('GET');
+
+$arr_data = json_decode($response->getBody(), true);
+$taiex = $arr_data['taiex'];
+
+$return_data = [
+    'buy' => $taiex['index'],
+    'sell' =>  $taiex['volume'],
+    'buy1' => substr($taiex['index'], -1),
+    'sell1' => substr(explode('.', $taiex['volume'])[0], -1)
+];
+
+echo json_encode($return_data);
